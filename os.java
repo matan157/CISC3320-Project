df91861c3a430f6bbead81b649e7209e6b420ef3
Matan Uchen <matan.uchen@gmail.com>
2016-03-29 18:51:29 -0400

    Finish initial setup.

---
+++ os.java
@@ -0,0 +1,16 @@
+public class OS {
+
+	public OS {}
+
+	public static void Crint (int[] a, int[] p) {}
+
+	public static void Dskint (int[] a, int[] p) {}
+	
+	public static void Drmint (int[] a, int[] p) {}
+
+	public static void Tro  (int[] a, int[] p) {}
+
+	public static void Svc (int[] a, int[] p) {}
+
+}
+
