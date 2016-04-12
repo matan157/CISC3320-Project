10c0d14709603594b2dc2dedad9dc44bcf2638c6
matan157 <matan.uchen@gmail.com>
2016-04-12 11:03:25 -0400

    Added state class

---
+++ Classes/State.java
@@ -0,0 +1,10 @@
+public class State {
+	public static final int NEW = 0;
+	public static final int READY = 1;
+	public static final int RUNNING = 2;
+	public static final int WAITING = 3;
+	public static final int HALTED = 4;
+
+	public State() {
+	}
+}
