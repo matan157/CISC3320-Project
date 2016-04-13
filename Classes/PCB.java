15b8c767d5e2bd75eac3c6db73357066ae4e46c5
Matan Uchen <matan.uchen@gmail.com>
2016-04-13 11:42:18 -0400

    Edit PCB to a fuller extent

--- Classes/PCB.java
+++ Classes/PCB.java
@@ -1,49 +1,86 @@
 public class PCB {
     
     // State constants
-    private static final int NEW = 0;
-    private static final int READY = 1;
-    private static final int RUNNING = 2;
-    private static final int WAITING = 3;
-    private static final int HALTED = 4;
-    
     private static int PID; // Process ID
-    private State state; // 0 - 4 will represent states
-    private int PC; // Program Counter
+	private int jobSize;
+	private int timeArrived;
+	private int lastTimeProcessing;
+	private int cpuTimeUsed;
+	private int maxCpuTime;
+	private int ioPending;
+	private int priority;
+	private int address;
+	private int timesBlocked;
+	private boolean blocked;
+	private boolean latched;
+	private boolean inCore;
+	private boolean terminated;
+	    
+	public PCB(int PID, int priority, int jobSize, int maxCpuTime, int timeArrived) {
+		this.PID = PID;
+		this.priority = priority;
+		this.jobSize = jobSize;
+		this.maxCpuTime = maxCpuTime;
+		this.timeArrived = timeArrived;
+		cpuTimeUsed = 0;
+		ioPending = 0;
+		blocked = false;
+		latched = false;
+		inCore = false;
+		terminated = false;
+		timesBlocked = 0;
+		address = -1;
+	}
+	// GETTERS 
+	public int getPID() { return PID; } 
+	public int getTimesBlocked() { return timesBlocked; }
+	public int getTimeArrived() { return timeArrived; }
+	public int getJobSize() { return jobSize; }
+	public int getAddress() { return address; }
+	public int getMaxCpuTime() { return maxCpuTime; }
+	public int getIoCount() { return ioPending; }
+	public int getCpuTimeUsed() { return cpuTimeUsed; }
+	public int getCpuTimeLeft() { return maxCpuTime - cpuTimeUsed; }
+	public int getPriority() { return priority; }
+	public boolean isBlocked() { return blocked; }
+	public boolean isInCore() { return inCore; }
+	public boolean isTerminated() { return terminated; }
+	public boolean isLatched() { return latched; }
     
-    // TODO: Add CPU scheduling
-    // TODO: Add Memory Management
-    
-    public PCB(int PID, State state, int PC) {
-        // Check if state is valid.
-        if (state > 4 || state < 0) {
-            throw new RuntimeException("State is invalid.");
-        }
-        this.PID = PID;
-        this.state = state;
-        this.PC = PC;
-    }
-    
-    //////// Getters and Setters ////////////
-    //////// Possibly setters for everything.
-    
-    // Returns PID as INT
-    public int getPID() {
-        return this.PID;
-    }
-    
-    // Returns state as INT
-    public State getState() {
-        return this.state;
-    }
-    
-    // Returns PC as INT
-    public int getPC() {
-        return this.PC;
-    }
-    
-    // Set PC
-    public void setPC(int address) {
-        this.PC = address;
-    }
+	// SETTERS
+	public void setAddress(int address) {
+		this.address = address; 
+	}
+
+	public void setLastTimeProcessing(int lastTimeProcessing) {
+		this.lastTimeProcessing = lastTimeProcessing;
+	}
+
+	public void calculateTimeProcessed(int currentTime) {
+		cpuTimeUsed += currentTime - lastTimeProcessing;
+	}
+
+	public void incTimeBlocked() { timesBlocked++; }
+	public void incIoCount() { ioPending++; }
+	public void decIoCount() { ioPending--; }
+	public void blockJob() {
+		blocked = true;
+	}
+	public void unblockJob() {
+		blocked = false;
+	}
+	public void latchJob() {
+		latched = true;
+	}
+	public void unlatchJob() {
+		latched = false;
+	}
+	public void removeInCore() {
+		inCore = false;
+	}
+	public void terminateJob() {
+		terminated = true;
+	}
+
+	// TODO add print method
 }
