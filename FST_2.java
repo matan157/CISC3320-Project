c8c7757a6aac66988cf84e50b0ca551d9a7d7e59
PingwinZloty <nicogala93@gmail.com>
2016-05-22 11:56:16 -0400

    Second free space table with comments.

--- FST_2.java
+++ FST_2.java
@@ -14,17 +14,25 @@ public class FST_2 {
     
     // Implementing BEST FIT
     public int findSpaceForJob(PCB job) {
-        int jobSize = job.getJobSize();
-        Collections.sort(FreeSpaceTable);
+        int jobSize = job.getJobSize(); // Save state of current job size. 
+        Collections.sort(FreeSpaceTable); 
+	// Iterate through list in acsending order, as soon as the first 
+	// smallest job that is found which fits inside of the free space 
+	// table, the address to the space is returned. 
         for(FreeSpace fs : FreeSpaceTable) {
+	    // Save the state of the chunksize and chunkaddress since they will update. 
+	    // upon iteration. 
             int chunkSize = fs.getSize();
             int chunkAddress = fs.getAddress();
             if(chunkSize >= jobSize) {
+		// Computations to determine where to place job in the currently free chunk. 
                 fs.setAddress(chunkAddress + jobSize);
                 fs.setSize(chunkSize - jobSize);
-                if(fs.getSize() == 0)
+		// If a portion in the free space table is zero, remove the free space chunk. 
+                if(fs.getSize() == 0) 
                     FreeSpaceTable.remove(fs);
-                job.setAddress(chunkAddress);
+		// Place job in the smallest chunk it can fit. 
+                job.setAddress(chunkAddress); 
                 return chunkAddress;
             }
         }
@@ -32,19 +40,21 @@ public class FST_2 {
         return -1;
     }
     
-    
+   /* Get a job out of memory. */ 
     public void addSpace(PCB job) {
+	// Save state of job address and its size. 
         int jobAddress = job.getAddress();
         int jobSize = job.getJobSize();
+	// If job is next to another freespace chunk, combine them. 
         for(FreeSpace fs : FreeSpaceTable) {
             int chunkAddress = fs.getAddress();
             int chunkSize = fs.getSize();
             
-            if(chunkAddress + chunkSize == jobAddress) {
+            if(chunkAddress + chunkSize == jobAddress) { // Job After 
                 fs.setSize(chunkSize + jobSize);
                 job.setAddress(-1);
                 return;
-            } else if(jobSize + jobAddress == chunkAddress) {
+            } else if(jobSize + jobAddress == chunkAddress) { // Job Before
                 fs.setAddress(jobAddress);
                 fs.setSize(jobSize + chunkSize);
                 job.setAddress(-1);
